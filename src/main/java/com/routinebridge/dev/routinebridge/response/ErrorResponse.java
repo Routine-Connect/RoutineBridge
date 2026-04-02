e988127f70e63965aa8187789df1625e902a0546
문광희 <mgh7020@gmail.com>
2026-04-02 21:37:43 +0900

    feat : 공통 응답 포맷 및 에러코드 목록 작업

---
+++ src/main/java/com/routinebridge/dev/routinebridge/response/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.routinebridge.dev.routinebridge.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private String code;
+    private String message;
+}
