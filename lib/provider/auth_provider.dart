6e6a4656a45e6c5d355575c93ae795e6d5a66dc3
adward27 <whalsrnr2741@gmail.com>
2026-04-15 20:05:14 +0900

    chore : 오타 수정

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import '../service/auth.service.dart';
+import '../service/auth_service.dart';
 
 class AuthProvider with ChangeNotifier {
   final FlutterSecureStorage _storage = const FlutterSecureStorage();
