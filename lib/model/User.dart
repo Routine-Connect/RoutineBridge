2f287eaeafe54ce12e5df9851a20363d07f45cb7
adward27 <whalsrnr2741@gmail.com>
2026-04-20 23:39:42 +0900

    feat : profileImage 추가 및 유저 정보 변환, 수정 함수 선언

--- lib/model/User.dart
+++ lib/model/User.dart
@@ -4,10 +4,39 @@ class User {
   final String email;
   final String nickname;
   final String gender;
+  final String? profileImage;
 
   User({
     required this.id,
     required this.email,
     required this.nickname,
-    required this.gender});
+    required this.gender,
+    this.profileImage,
+    });
+
+    factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      email: json['email'],
+      nickname: json['nickname'],
+      gender: json['gender'],
+      profileImage: json['profile_image'],
+    );
+  }
+
+  User copyWith({
+    int? id,
+    String? email,
+    String? nickname,
+    String? gender,
+    String? profileImage,
+  }) {
+    return User(
+      id: id ?? this.id,
+      email: email ?? this.email,
+      nickname: nickname ?? this.nickname,
+      gender: gender ?? this.gender,
+      profileImage: profileImage ?? this.profileImage, // 이미지 변경 시 덮어씌움
+    );
+  }
 }
