ad4c33f8cdc16644b00b9c3375a2f424838eeb63
adward27 <whalsrnr2741@gmail.com>
2026-04-23 06:33:24 +0900

    feat : 달력 데이터 로딩 (intl) / RoutineProvider 생성자 선언

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'provider/auth_provider.dart';
 import 'provider/user_provider.dart';
+import 'provider/routine_provider.dart';
 import 'screen/splash_screen.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+Future<void> main() async { 
+  // 플러터 엔진 초기화 및 한국어 달력 데이터 로딩
+  WidgetsFlutterBinding.ensureInitialized(); 
+  await initializeDateFormatting('ko_KR', null); 
+
   runApp(
-    // 💡 앱 전체에서 쓸 Provider들을 여기서 묶어서 등록해 줍니다.
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
-        ChangeNotifierProvider(create: (_) => UserProvider()),
+        ChangeNotifierProvider(create: (_) => UserProvider()), 
+        ChangeNotifierProvider(create: (_) => RoutineProvider()),
       ],
       child: const MyApp(),
     ),
