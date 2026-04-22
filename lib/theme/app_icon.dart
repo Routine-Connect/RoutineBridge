cd0399dc6b50b2855f6b39e9d164fdaa947cb6ca
adward27 <whalsrnr2741@gmail.com>
2026-04-23 06:55:43 +0900

    feat : 아이콘 리스트 분리

---
+++ lib/theme/app_icon.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppIcons {
+  // 🚀 앱 전체에서 공통으로 사용할 20개의 루틴 아이콘 리스트
+  static const List<IconData> routineIcons = [
+    Icons.star, Icons.fitness_center, Icons.directions_run, Icons.menu_book,
+    Icons.self_improvement, Icons.apple, Icons.computer, Icons.bed,
+    Icons.brush, Icons.music_note, Icons.shopping_cart, Icons.cleaning_services,
+    Icons.local_cafe, Icons.wb_sunny, Icons.nights_stay, Icons.pets,
+    Icons.attach_money, Icons.flight, Icons.favorite, Icons.water_drop,
+  ];
+}
