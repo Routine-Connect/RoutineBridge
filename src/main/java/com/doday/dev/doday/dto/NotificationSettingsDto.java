fecf115e09a8046d8898ecfdb2230f97ff8f40f5
문광희 <mgh7020@gmail.com>
2026-06-21 16:45:52 +0900

    feat : DTO 입력값 검증 추가
    - @NotNull 검증 추가
    - 기타 DTO 검증 어노테이션 점검

--- src/main/java/com/doday/dev/doday/dto/NotificationSettingsDto.java
+++ src/main/java/com/doday/dev/doday/dto/NotificationSettingsDto.java
@@ -4,6 +4,8 @@ import lombok.Data;
 
 @Data
 public class NotificationSettingsDto {
+
+
     private Boolean isPushEnabled;
     private Boolean isRoutineNotiEnabled;
     private Boolean isMarketingEnabled;
--- src/main/java/com/doday/dev/doday/dto/RoutineOrderDto.java
+++ src/main/java/com/doday/dev/doday/dto/RoutineOrderDto.java
@@ -2,9 +2,14 @@ package com.doday.dev.doday.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class RoutineOrderDto {
 
+    @NotNull(message = "루틴 ID는 필수입니다.")
     private Long id;
+
+    @NotNull(message = "순서는 필수입니다.")
     private Integer sortOrder;
 }
